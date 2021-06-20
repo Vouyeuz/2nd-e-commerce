ede4532a4746f8d3614f4cef46f7eea5ec261dc0
Vouyeuz <fahrizal.indi@gmail.com>
2021-06-20 18:07:17 +0700

    Membuat clone

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,42 +1,79 @@
-import React from "react";
-import {
-  Card,
-  CardMedia,
-  CardContent,
-  CardActions,
-  Typography,
-  IconButton,
-} from "@material-ui/core";
-import { AddShoppingCart } from "@material-ui/icons";
-import useStyle from "./styles";
+// import React from "react";
+// import {
+//   Card,
+//   CardMedia,
+//   CardContent,
+//   CardActions,
+//   Typography,
+//   IconButton,
+// } from "@material-ui/core";
+// import { AddShoppingCart } from "@material-ui/icons";
+// import useStyle from "./styles";
 
-const Product = ({ product }) => {
-  const classes = useStyle();
+// const Product = ({ product }) => {
+//   const classes = useStyle();
+
+//   return (
+//     <Card className={classes.root}>
+//       <CardMedia
+//         className={classes.media}
+//         image={product.media.source}
+//         title={product.name}
+//       />
+//       <CardContent>
+//         <div className={classes.cardContent}>
+//           <Typography variant="h6" gutterBottom>
+//             {product.name}
+//           </Typography>
+//           <Typography variant="h6">
+//             {product.price.formatted_with_symbol}
+//           </Typography>
+//         </div>
+//         <Typography
+//           dangerouslySetInnerHTML={{ __html: product.description }}
+//           variant="body2"
+//           color="textSecondary"
+//         />
+//       </CardContent>
+//       <CardActions disableSpacing className={classes.cardActions}>
+//         <IconButton aria-label="Add to Cart">
+//           <AddShoppingCart />
+//         </IconButton>
+//       </CardActions>
+//     </Card>
+//   );
+// };
+
+// export default Product;
+
+
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+import useStyles from './styles';
+
+const Product = ({ product, onAddToCart }) => {
+  const classes = useStyles();
+
+  const handleAddToCart = () => onAddToCart(product.id, 1);
 
   return (
     <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        image={product.media.source}
-        title={product.name}
-      />
+      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
       <CardContent>
         <div className={classes.cardContent}>
-          <Typography variant="h6" gutterBottom>
+          <Typography gutterBottom variant="h5" component="h2">
             {product.name}
           </Typography>
-          <Typography variant="h6">
-            {product.price.formatted_with_symbol}
+          <Typography gutterBottom variant="h5" component="h2">
+            ${product.price.formatted}
           </Typography>
         </div>
-        <Typography
-          dangerouslySetInnerHTML={{ __html: product.description }}
-          variant="body2"
-          color="textSecondary"
-        />
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart">
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
           <AddShoppingCart />
         </IconButton>
       </CardActions>
@@ -45,3 +82,4 @@ const Product = ({ product }) => {
 };
 
 export default Product;
+
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,46 +1,19 @@
-import React from "react";
-import Grid from "@material-ui/core/Grid";
+// import React from "react";
+// import Grid from "@material-ui/core/Grid";
 
-import Product from "./Product/Product";
-import useStyles from "./styles";
+// import Product from "./Product/Product";
+// import useStyles from "./styles";
 
-const Products = ({ products }) => {
-  const classes = useStyles();
-
-  return (
-    <main className={classes.content}>
-      <div className={classes.toolbar} />
-      <Grid container justify="center" spacing={4}>
-      {products.map((product) => (
-          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
-          </Grid>
-        ))}
-      </Grid>
-    </main>
-  );
-};
-
-export default Products;
-
-// import React from 'react';
-// import Grid from '@material-ui/core/Grid';
-
-// import Product from './Product/Product';
-// import useStyles from './styles';
-
-// const Products = ({ products, onAddToCart }) => {
+// const Products = ({ products }) => {
 //   const classes = useStyles();
-
-//   if (!products.length) return <p>Loading...</p>;
 
 //   return (
 //     <main className={classes.content}>
 //       <div className={classes.toolbar} />
 //       <Grid container justify="center" spacing={4}>
-//         {products.map((product) => (
+//       {products.map((product) => (
 //           <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-//             <Product product={product} onAddToCart={onAddToCart} />
+//             <Product product={product} />
 //           </Grid>
 //         ))}
 //       </Grid>
@@ -49,4 +22,31 @@ export default Products;
 // };
 
 // export default Products;
+
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import Product from './Product/Product';
+import useStyles from './styles';
+
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
+
+  if (!products.length) return <p>Loading...</p>;
+
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
 
